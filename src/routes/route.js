9db45472a1abef4bc75d6461f50c9a2e694b9c35
TariqHusainKhan <61875008+TariqHusainKhan@users.noreply.github.com>
2022-04-25 10:27:41 +0530

    assignmet complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,16 +2,11 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+c
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+fffbgbfg
 
-
-
-
+dgfybrt
 router.post("/createBook", BookController.createBook  )
 
 
